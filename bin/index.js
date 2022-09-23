4389262612411992813131c2db2860c698fcd603
V.spider <mahmudhassen729@gmail.com>
2022-09-23 18:58:40 +0300

    logging my cv

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+console.log(`
+Name: Mahmud Hassen
+
+
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque tempora sunt debitis odit necessitatibus numquam optio libero aspernatur sint ipsam nam, natus eius modi aut? Officiis error facilis eaque totam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque tempora sunt debitis odit necessitatibus numquam optio libero aspernatur sint ipsam nam, natus eius modi aut? Officiis error facilis eaque totam?
+
+Github: https://www.github.com
+twitter: https://www.github.com
+facebook: https://www.github.com
+instgram: https://www.github.com
+`);
+
